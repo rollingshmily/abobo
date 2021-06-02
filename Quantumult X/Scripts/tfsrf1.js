51b35da086083e12020d3b2597b0801d87259da5
AD <59598230+Zhuliyer@users.noreply.github.com>
2021-06-02 23:36:05 +0800

    Create tfsrf1.js

---
+++ Quantumult X/Scripts/tfsrf1.js
@@ -0,0 +1,18 @@
+/*
+微信公众号：ios黑科技
+官方网站：s7aa.cn
+QX:
+#TF输入法解锁会员
+^http:\/\/api\.chuangqi\.store\/.+ url script-response-body tfsrf.js
+[mitm]
+hostname = api.chuangqi.store
+TF输入法商店搜索下载
+https://apps.apple.com/cn/app/id1537722262
+*/
+
+var obj = JSON.parse($response.body);
+obj.data.vip_end_time = "永久会员";
+obj.data.isvip = 1;
+obj.data.is_p = 1;
+
+$done({body: JSON.stringify(obj)}); 
